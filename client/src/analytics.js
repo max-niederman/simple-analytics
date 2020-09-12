848228846bfb1e0e8022912f56d5eaa316aba1e9
Max Niederman <max@maxniederman.com>
2020-09-11 23:01:51 -0700

    feat(client): created client package

---
+++ client/src/analytics.js
@@ -0,0 +1,9 @@
+const analytics = async (apiBase) => {
+  await fetch(`${apiBase}/tracker`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ url: location.href, screenWidth: screen.width }),
+  });
+};
